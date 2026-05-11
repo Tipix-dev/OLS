67d600724be82c8da0a4a6f6e78f8f1dbcbb19fc
Artem <artemkolba321@gmail.com>
2026-05-11 08:19:22 +0700

    Fix: minor

--- src/lib/env.sh
+++ src/lib/env.sh
@@ -6,21 +6,21 @@ DEBUG_LOG_FILE="$OLS_DIR/logs/debug.log"
 
 export OLS_VERSION=$(cat "$OLS_DIR/.version" || echo "Unknown")
 info() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][INFO] $@" >> "$LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][INFO] $*" >> "$LOG_FILE"
 }
 
 warn() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][WARN] $@" >> "$LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][WARN] $*" >> "$LOG_FILE"
 }
 
 ee() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][EE] $@" >> "$LOG_FILE" && \
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][EE] $@" >> "$DEBUG_LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][EE] $*" >> "$LOG_FILE" && \
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][EE] $*" >> "$DEBUG_LOG_FILE"
     exit 1
 }
 
 panic() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][PANIC] $@" >> "$LOG_FILE" && \
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][PANIC] $@" >> "$DEBUG_LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][PANIC] $*" >> "$LOG_FILE" && \
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OLS]["$(basename "$0")"][PANIC] $*" >> "$DEBUG_LOG_FILE"
     exit 1
 }
